86bd01880bd3181278b2a43e32fcf57ebffb3c05
Rui Coelho <ruicoelho@ua.pt>
2019-11-01 15:37:17 +0000

    Update navbar

--- TPW_Proj1/urls.py
+++ TPW_Proj1/urls.py
@@ -21,7 +21,7 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('contact/', views.contact, name='contact'),
+    path('find/', views.contact, name='find'),
     path('about/', views.about, name='about'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
